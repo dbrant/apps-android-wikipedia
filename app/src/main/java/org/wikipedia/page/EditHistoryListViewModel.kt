eb92c0b92da0cca653cc0c93ba4238312bbc56cb
Dmitry Brant <me@dmitrybrant.com>
2022-02-22 10:08:09 -0500

    PagingSource.

--- app/src/main/java/org/wikipedia/page/EditHistoryListViewModel.kt
+++ app/src/main/java/org/wikipedia/page/EditHistoryListViewModel.kt
@@ -3,13 +3,17 @@ package org.wikipedia.page
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.wikipedia.dataclient.ServiceFactory
 import org.wikipedia.dataclient.WikiSite
-import org.wikipedia.dataclient.mwapi.MwQueryPage.Revision
+import org.wikipedia.dataclient.mwapi.MwQueryPage
 import org.wikipedia.dataclient.restbase.DiffResponse
 import org.wikipedia.util.Resource
 import org.wikipedia.util.Resource.Success
@@ -17,13 +21,38 @@ import org.wikipedia.util.log.L
 
 class EditHistoryListViewModel : ViewModel() {
 
-    val editHistoryListData = MutableLiveData<Resource<List<Revision>>>()
+    val editHistoryListData = MutableLiveData<Resource<List<MwQueryPage.Revision>>>()
 
     private val handler = CoroutineExceptionHandler { _, throwable ->
         L.e(throwable)
     }
 
-    fun fetchData(pageTitle: PageTitle) {
+
+
+    var pageTitle: PageTitle? = null
+
+
+
+
+    val flow = Pager(PagingConfig(pageSize = 20)) {
+        EditHistoryPagingSource()
+    }
+
+
+
+    fun fetchData(title: PageTitle) {
+
+        if (pageTitle != null) {
+            return
+        }
+
+        pageTitle = title
+
+
+
+
+
+
         viewModelScope.launch(handler) {
             withContext(Dispatchers.IO) {
                 val response = ServiceFactory.get(WikiSite.forLanguageCode(pageTitle.wikiSite.languageCode))
@@ -66,4 +95,28 @@ class EditHistoryListViewModel : ViewModel() {
         }
         return diffSize
     }
+
+
+    class EditHistoryPagingSource(
+            val pageTitle: PageTitle
+    ) : PagingSource<String, MwQueryPage.Revision>() {
+        override suspend fun load(params: LoadParams<String>): LoadResult<String, MwQueryPage.Revision> {
+            return try {
+                val response = ServiceFactory.get(WikiSite.forLanguageCode(pageTitle.wikiSite.languageCode))
+                        .getEditHistoryDetails(pageTitle.prefixedText)
+                LoadResult.Page(response.query!!.pages?.get(0)?.revisions!!, null, response.continuation?.continuation)
+            } catch (e: Exception) {
+                LoadResult.Error(e)
+            }
+        }
+
+        override fun getRefreshKey(state: PagingState<String, MwQueryPage.Revision>): String? {
+            return null
+        }
+    }
+
+
+
+
+
 }
