c76c015889e63318c60cac261431ca179b7659ff
Dmitry Brant <me@dmitrybrant.com>
2024-03-30 21:06:23 -0400

    Don't keep unnecessary fields.

--- app/src/main/java/org/wikipedia/dataclient/okhttp/OkHttpConnectionFactory.kt
+++ app/src/main/java/org/wikipedia/dataclient/okhttp/OkHttpConnectionFactory.kt
@@ -20,15 +20,12 @@ object OkHttpConnectionFactory {
     val CACHE_CONTROL_MAX_STALE = CacheControl.Builder().maxStale(Int.MAX_VALUE, TimeUnit.SECONDS).build()
     val CACHE_CONTROL_NONE = CacheControl.Builder().build()
 
-    private const val CACHE_DIR_NAME = "okhttp-cache"
-    private const val NET_CACHE_SIZE = (64 * 1024 * 1024).toLong()
-    private val NET_CACHE = Cache(File(WikipediaApp.instance.cacheDir, CACHE_DIR_NAME), NET_CACHE_SIZE)
     val client = createClient()
 
     private fun createClient(): OkHttpClient {
         val builder = OkHttpClient.Builder()
                 .cookieJar(SharedPreferenceCookieManager.instance)
-                .cache(NET_CACHE)
+                .cache(Cache(File(WikipediaApp.instance.cacheDir, "okhttp-cache"), (64 * 1024 * 1024).toLong()))
                 .readTimeout(20, TimeUnit.SECONDS)
                 .addInterceptor(UnsuccessfulResponseInterceptor())
                 .addNetworkInterceptor(CacheControlInterceptor())
