de15dfe1125cfa0476ca44d0b8e9803cfac9ad6b
Dmitry Brant <me@dmitrybrant.com>
2024-05-01 13:13:46 -0400

    Detect if controller was disconnected.

--- app/src/main/java/org/wikipedia/page/tts/Tts.kt
+++ app/src/main/java/org/wikipedia/page/tts/Tts.kt
@@ -97,6 +97,10 @@ object Tts {
     }
 
     private fun speak(context: Context) {
+        if (mediaController?.isConnected == false) {
+            mediaController?.release()
+            mediaController = null
+        }
         if (mediaController == null) {
             val sessionToken = SessionToken(context, ComponentName(context, PlaybackService::class.java))
             val controllerFuture = MediaController.Builder(context, sessionToken).buildAsync()
