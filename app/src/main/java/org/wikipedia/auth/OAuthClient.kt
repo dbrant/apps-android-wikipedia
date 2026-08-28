6139ec98dd1e474edf83cae2eb1062e47f988779
Dmitry Brant <me@dmitrybrant.com>
2026-08-28 09:06:15 -0400

    Clean up a bit.

--- app/src/main/java/org/wikipedia/auth/OAuthClient.kt
+++ app/src/main/java/org/wikipedia/auth/OAuthClient.kt
@@ -5,36 +5,25 @@ import android.content.Intent
 
 interface OAuthClient {
 
-    // Startup initialization
     fun initialize()
 
-    // Try to get an access token
     suspend fun getAccessToken(): String?
 
-    // Try to refresh an access token
     suspend fun synchronizedRefreshAccessToken(): String
 
-    // Return true if there are tokens
     fun isLoggedIn(): Boolean
 
-    // Start a login redirect
     fun startLogin(pendingIntent: PendingIntent)
 
-    // Complete a login
     suspend fun finishLogin(intent: Intent)
 
-    // Start a logout redirect
     fun startLogout(launchAction: (i: Intent) -> Unit)
 
-    // Complete a logout
     fun finishLogout()
 
-    // Allow the app to clear its login state after certain errors
     fun clearLoginState()
 
-    // For testing, make the access token act expired
     fun expireAccessToken()
 
-    // For testing, make the refresh token act expired
     fun expireRefreshToken()
 }
